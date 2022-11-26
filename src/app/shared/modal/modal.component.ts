4fde69878e54bdf722ff400c2d289c688f6951a5
gabrielcasetta <gabrielcasetta@protonmail.com>
2022-11-26 12:18:17 -0300

    Injecting services, open/close modal, ID system

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  //providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
 
-  constructor(public modal: ModalService) {
-    console.log(this.modal.visible);
-  }
+  @Input() modalID = '';
+
+  constructor(public modal: ModalService) {  }
 
   ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
