1f4a14cc5b2548bcb69bce5e749923af473f90ff
gabrielcasetta <gabrielcasetta@protonmail.com>
2022-12-27 23:52:43 -0300

    Component Design

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -11,9 +11,14 @@ export class ModalComponent implements OnInit {
 
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {  }
+  constructor(
+    public modal: ModalService,
+    public el: ElementRef) {
+      //console.log(el);
+  }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal() {
